913af8fb640eadf174ddd2c14cf86c7390407db9
rae <josephonofiok08@gmail.com>
2026-02-25 03:23:40 +0100

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/toaster"
+import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "./pages/home";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+
+        {/* 🔥 THIS IS THE FIX */}
+        <Router base="/void-walker">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+
       </TooltipProvider>
     </QueryClientProvider>
   );
